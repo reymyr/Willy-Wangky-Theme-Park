256fdb258b4bb52bf5ad8d3b01c2fde2e7c3fe09
reymyr <reyhan_emyr@hotmail.com>
2020-11-20 13:31:54 +0700

    update adt.h

--- src/adt.h
+++ src/adt.h
@@ -4,11 +4,6 @@
 #include "boolean.h"
 #include "arrAction/arrAction.h"
 #include "arrMaterial/arrMaterial.h"
-#include "jam/jam.h"
-#include "point/point.h"
-#include "map/map.h"
-#include "prioQueuePengunjung/prioQueuePengunjung.h"
-#include "wahana/wahana.h"
 #include "string_production/mesinkata.h"
 #include "string_production/mesinkar.h"
 
