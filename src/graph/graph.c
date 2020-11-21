ac7777f7eb444934552a5af8fdeabd1c302bd901
rayfazt <rayfazt@gmail.com>
2020-11-22 01:43:54 +0700

    header graph

--- src/graph/graph.c
+++ src/graph/graph.c
@@ -3,3 +3,10 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "graph.h"
+
+void CreateEmptyGraph (Graph *G)
+/* I.S. G sembarang */
+/* F.S. Terbentuk graph G dengan First(*G) = Nil */
+{
+    First(*G) = Nil;
+}
--- src/graph/graph.h
+++ src/graph/graph.h
@@ -4,5 +4,31 @@
 #define graph_H
 
 #include "../boolean.h"
+#include "../peta/peta.h"
+
+#define Nil NULL
+
+typedef struct tNodeGraph *address;
+typedef struct tNodeGraph {
+    Peta map;
+    address next;
+    address prev;
+} NodeGraph;
+
+typedef struct {
+    address First;
+} Graph;
+
+/*** Selektor, Note: masi belom bener kayanya ***/
+#define Map(G) (G)->map
+#define First(G) ((G).First)
+#define Next(G) (G)->next
+#define Prev(G) (G)->prev
+
+
+
+void CreateEmptyGraph (Graph *G);
+/* I.S. G sembarang */
+/* F.S. Terbentuk graph G dengan First(*G) = Nil */
 
 #endif
