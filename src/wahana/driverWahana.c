1ef06b8b535f337cd4ea01b7cc01155a14169e81
reymyr <reyhanemyr51@gmail.com>
2020-11-29 23:37:20 +0700

    update driver wahana

--- src/wahana/driverWahana.c
+++ src/wahana/driverWahana.c
@@ -9,21 +9,22 @@ int main()
 {
     ArrListWahanaUpg A;
     WU_CreateEmpty(&A);
-    loadwahanahistory("../../files/wahanahistory.txt",&A);
+    loadwahanahistory("testwahanahistory.txt",&A);
 
     for (int i=0;i<A.Neff;i++){
+        printf("\n");
         AW_detailWahana(Info(First(A.Tab[i])),A);
     }
 
-
     ArrWahana AW;
+    printf("\nMembaca file 'testwahana.txt'\n");
     AW_BacaFile(&AW, "testwahana.txt");
 
-    AW_ListNamaWahana(AW); printf("\n");
+    printf("List nama wahana yang terbaca:\n");
+    AW_ListNamaWahana(AW); printf("\n\n");
 
-    AW_detailWahana(Info(First(WU_Info(A,0))),A);
-
-    savewahanahistory("testwahanahist.txt",A);
+    printf("Menyimpan history di file 'savedWahanHistory.txt'...\n");
+    savewahanahistory("savedWahanHistory.txt",A);
 
 
     return 0;
